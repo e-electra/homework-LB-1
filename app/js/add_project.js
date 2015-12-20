64ac9fe2ce36cb251ca49d18627649a88c2d430a
ErmolaevaEkaterina <ermolaevakatty@yandex.ru>
2015-12-20 23:54:57 +0300

    fixed server response message

--- app/js/add_project.js
+++ app/js/add_project.js
@@ -35,18 +35,20 @@ var mainModule = (function (){
 		ev.preventDefault();
 
 		var form = $(this),
-			url = 'new_project.php',
+			url = 'http://localhost:8000/new_project.php',
 			defObj = _ajaxForm(form, url);
 		
 
 		if(defObj) {
 			defObj.done(function(answer) {
 			console.log(answer);
+			console.log(answer.status);
 			var successDiv = $('.success-mes'),
 				errorDiv = $('.error-mes');
-			if(answer.status === "OK"){
+			if(answer.status === "success"){
 				errorDiv.hide();
 				successDiv.html(answer.text).show();
+				$('form').trigger('reset');
 			} 	else {
 					successDiv.hide();				
 					errorDiv.html(answer.text).show();
@@ -70,6 +72,7 @@ var mainModule = (function (){
 			data: data,
 		}).fail(function(answer){
 			console.log('Проблемы в PHP');
+			//console.log(answer);
 			$('.error-mes').text('На сервере произошла ошибка!').show();
 		});
 
--- app/new_project.php
+++ app/new_project.php
@@ -1,18 +1,27 @@
 <?php
-
+	//header("Content-Type: application/json");
 	$name = $_POST['names'];
+	$file = $_FILES['my_file'];
+	$url = $_POST['links'];
+	$descr = $_POST['areas'];
+
+
 	$data = array();
 
 		
-	if ($name === '') {
+	if ($name === '' && $url === '' && $descr === '') {
 		$data['status'] = 'error';
 		$data['text'] = "Заполните имя!";
-	} else {
-		$data['mes'] = 'OK';
-		$data['text'] = "Вы не забыли заполнить имя";
+	} elseif ( $file && ($file['type'] != 'image/png' || $file['type'] != 'image/jpeg') ) {
+		$data['mes_file'] = 'Неправильный тип файла';
+		$data['mes_f_type'] = $file['type'];
+	}
+	 else {
+	 	$data['status'] = 'success';
+		$data['text'] = "Запрос прошел успешно";
 	}
 
-
+	//print_r($_POST);
 	header("Content-Type: application/json");
 	echo json_encode($data);
 	exit;
